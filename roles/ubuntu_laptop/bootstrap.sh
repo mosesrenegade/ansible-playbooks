9355fd31a0412404d7b9f16bd346adc0f8639176
Moses Frost <moses@moses.io>
2018-07-03 14:29:10 -0400

    First Commit -Signed

---
+++ roles/ubuntu_laptop/bootstrap.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+# Install Ansible and its dependencies before executing this role.
+
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
+apt install -y ssh python-apt ansible
+echo "localhost" >> /etc/ansible/hosts
