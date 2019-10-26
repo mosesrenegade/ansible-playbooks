a9c730a31d0cffba578cb609cb2e3c4bfc2e9ac1
Moses Frost <moses@moses.io>
2019-10-26 14:02:30 -0400

    Changes for Laptop

--- deploy.sh
+++ deploy.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
 
+if [ -z $( which ansible ) ]
+  then
+  sudo apt update -y
+  sudo apt install software-properties-common -y
+  sudo apt-add-repository --yes --update ppa:ansible/ansible -y
+  sudo apt install ansible -y
+fi
+
 sudo ansible-playbook -i inventory laptop.yml
